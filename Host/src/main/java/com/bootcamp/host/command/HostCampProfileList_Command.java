4e7e82541925f1f2b9acdda9aa2830d9fb8df2fd
EthanLee <lyj72011648@gmail.com>
2022-11-09 18:30:55 +0900

    오타 수정

--- Host/src/main/java/com/bootcamp/host/command/HostCampProfileList_Command.java
+++ Host/src/main/java/com/bootcamp/host/command/HostCampProfileList_Command.java
@@ -12,19 +12,17 @@ public class HostCampProfileList_Command implements BCCommand {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
-<<<<<<< HEAD
-	
-=======
+
 //		int hSeq = Integer.parseInt(request.getParameter("hSeq"));
 		// 로그인 화면 완성되면 hSeq값 받아오도록 바꿀 예정
->>>>>>> 297c868fc1611749bdd771693317b53d2505fb70
+
 		int hSeq = 1;
 		HostCampListDao dao = new HostCampListDao();
-		
+
 		ArrayList<regcampDto> dtos = dao.myCampProfileList(hSeq);
-		
+
 		request.setAttribute("host_camp_profile_list", dtos);
-		
+
 		// Host Main 화면에서 썸네일 담아줌 (사진, 캠핑장이름, 주소)
 
 	}
