d0eccb7290f185224a1199b4393af6573b35bbd1
kimjn132 <kjhi8309@gmail.com>
2022-11-13 03:43:34 +0900

    리뷰 리스트, 리뷰 상세, 리뷰 작성

---
+++ Client/src/main/java/com/bootcamp/client/command/ReviewerNameCommand.java
@@ -0,0 +1,33 @@
+package com.bootcamp.client.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.bootcamp.client.dao.BCBookDao;
+import com.bootcamp.client.dao.ReviewDao;
+import com.bootcamp.dto.regcampDto;
+import com.bootcamp.dto.reviewDto;
+
+public class ReviewerNameCommand implements BCCommand {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		String cId = (String) session.getAttribute("cId");
+		
+		ReviewDao dao = new ReviewDao();
+		reviewDto dto = dao.reviewerName(cId); // 다오한테 받은 bId를 dto에 보냄 
+
+		request.setAttribute("reviewer", dto);
+		
+	}
+
+	@Override
+	public Boolean execute1(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
